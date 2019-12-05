20321a1549dbbc0d282cf9caedca5a57b0c5a5fc
Alexandrov Vitaliy <aleksandrov.vetall@yandex.ru>
2019-12-06 01:30:36 +0300

    remove unused interface

--- hw1-testing-app/src/main/java/ru/otus/spring/service/TestingService.java
+++
@@ -1,9 +0,0 @@
-package ru.otus.spring.service;
-
-import ru.otus.spring.domain.Question;
-
-import java.util.List;
-
-public interface TestingService {
-    void runTesting(List<Question> questions);
-}
