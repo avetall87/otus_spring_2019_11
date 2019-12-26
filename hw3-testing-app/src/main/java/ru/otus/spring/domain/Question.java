4dcbeaf9ca2ffdc319ce1a4b483a592e5baa7764
Alexandrov Vitaliy <aleksandrov.vetall@yandex.ru>
2019-12-27 00:16:10 +0300

    hw3 finish

---
+++ hw3-testing-app/src/main/java/ru/otus/spring/domain/Question.java
@@ -0,0 +1,17 @@
+package ru.otus.spring.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Question {
+    private String question;
+    private List<Answer> answers;
+}
