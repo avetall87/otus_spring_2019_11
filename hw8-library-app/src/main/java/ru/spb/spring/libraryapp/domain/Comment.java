198965a9ae8b6f3db6ea6072357b039845dd0125
Alexandrov Vitaliy <aleksandrov.vetall@yandex.ru>
2020-02-16 04:00:18 +0300

    hw8 remove unused code

--- hw8-library-app/src/main/java/ru/spb/spring/libraryapp/domain/Comment.java
+++ hw8-library-app/src/main/java/ru/spb/spring/libraryapp/domain/Comment.java
@@ -4,15 +4,11 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.data.annotation.Id;
 
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class Comment {
-    @Id
-    private Long id;
-
     private String comment;
 }
