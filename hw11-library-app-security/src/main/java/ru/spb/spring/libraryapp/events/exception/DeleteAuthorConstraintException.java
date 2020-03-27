2afdcf2f4075389a5e1501af0e79f7b25f49ebf7
Alexandrov Vitaliy <aleksandrov.vetall@yandex.ru>
2020-03-27 03:01:11 +0300

    hw 15 revert unused project

--- hw11-library-app-security/src/main/java/ru/spb/spring/libraryapp/events/exception/DeleteAuthorConstraintException.java
+++
@@ -1,9 +0,0 @@
-package ru.spb.spring.libraryapp.events.exception;
-
-public class DeleteAuthorConstraintException extends RuntimeException {
-
-    public DeleteAuthorConstraintException(String message) {
-        super(message);
-    }
-
-}
--- hw11-library-app-security/src/main/java/ru/spb/spring/libraryapp/repository/BookDaoCustom.java
+++
@@ -1,8 +0,0 @@
-package ru.spb.spring.libraryapp.repository;
-
-import ru.spb.spring.libraryapp.domain.Book;
-
-public interface BookDaoCustom {
-
-    void update(Book book);
-}
