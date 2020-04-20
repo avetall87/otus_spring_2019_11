bc46d1762301c080622820344a6f626426e08e29
Alexandrov Vitaliy <aleksandrov.vetall@yandex.ru>
2020-04-21 02:02:15 +0300

    remove files

--- hw17-docker/src/main/java/ru/spb/spring/libraryapp/events/exception/DeleteBookConstraintException.java
+++
@@ -1,9 +0,0 @@
-package ru.spb.spring.libraryapp.events.exception;
-
-public class DeleteBookConstraintException extends RuntimeException {
-
-    public DeleteBookConstraintException(String message) {
-        super(message);
-    }
-
-}
--- hw17-docker/src/main/java/ru/spb/spring/libraryapp/repository/AuthorDaoCustom.java
+++
@@ -1,7 +0,0 @@
-package ru.spb.spring.libraryapp.repository;
-
-import ru.spb.spring.libraryapp.domain.Author;
-
-public interface AuthorDaoCustom {
-    void update(Author author);
-}
